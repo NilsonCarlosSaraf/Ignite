4000a00d4daa863c972501f2121c8de5139fa6f7
Saraf <nilson.carlossaraf@exxonmobil.com>
2024-01-03 10:59:47 -0300

    update branch

--- nodejs/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
+++ nodejs/01-fundamentos-nodejs/streams/fake-upload-to-http-stream.js
@@ -1,13 +1,13 @@
 import { Readable } from 'node:stream'
 
 class OneToHundredStream extends Readable {
-    index = 2
+    index = 1
 
     _read() {
         const i = this.index++
 
         setTimeout(() => {
-            if (i > 100) {
+            if (i > 5) {
                 this.push(null)
             } else {
                 const buf = Buffer.from(String(i))
@@ -22,4 +22,8 @@ fetch('http://localhost:3334', {
     method: 'POST',
     body: new OneToHundredStream(),
     duplex: 'half',
+}).then(response => {
+    return response.text()
+}).then(data => {
+    console.log(data)
 })
