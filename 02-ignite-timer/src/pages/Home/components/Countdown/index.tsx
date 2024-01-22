5bed2d05bf0b21706efd2d140262bbaea3d14c76
Saraf <nilson.carlossaraf@exxonmobil.com>
2024-01-22 12:29:47 -0300

    update

--- 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
+++ 02-ignite-timer/src/pages/Home/components/Countdown/index.tsx
@@ -1,6 +1,53 @@
+import { useEffect, useState } from "react";
 import { CountdownContainer, Separator } from "./styles";
+import { differenceInSeconds } from "date-fns";
 
-export function Countdown() {
+interface CountdownProps {
+  activeCycle: any;
+  setCycles: any;
+  activeCycleId: any;
+}
+
+export function Countdown({ activeCycle, setCycles, activeCycleId }) {
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+
+  useEffect(() => {
+    let interval: number;
+
+    if (activeCycle) {
+      interval = setInterval(() => {
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
+        );
+
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id === activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          );
+
+          setAmountSecondsPassed(totalSeconds);
+
+          clearInterval(interval);
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
+      }, 1000);
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [activeCycle, totalSeconds, activeCycleId]); //sempre que uma variavel externa ao useEffect for usada, ela precisa ser implementada como dependencia do useEffect, como ao lado. Isso obviamente implica que sempre que ela mudar o useEffect sera ativo
+
   return (
     <CountdownContainer>
       <span>{minutes[0]}</span>
--- 02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ 02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,6 +1,27 @@
+import { useForm } from "react-hook-form";
 import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
+import * as zod from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export function NewCycleForm() {
+  const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod
+      .number()
+      .min(1, "O ciclo precisa ser de no minimo 5 minutos.")
+      .max(60, "O ciclo precisa ser de no maximo 60 minutos."),
+  });
+
+  type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolverr(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
