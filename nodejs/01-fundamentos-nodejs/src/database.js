cfe52e8da26bb81d4158e3d3866b48b7f4fe8efc
NilsonCarlosSaraf <sarafnilson@gmail.com>
2024-02-05 06:58:56 -0300

    update

--- nodejs/01-fundamentos-nodejs/src/database.js
+++ nodejs/01-fundamentos-nodejs/src/database.js
@@ -1,7 +1,22 @@
 import fs from 'node:fs/promises'
 
+const databasePath = new URL('../db.json', import.meta.url)
+
 export class Database {
     #database = {} //#database se inserio o # antes do database(vai precisar isnerir em todos os databases desse arquivo) o database se tornara uma propriedade privada, nao podendo ser acessado em outros arquivos
+
+    constructor() {
+        fs.readFile(databasePath, 'utf-8').then(data => {
+            this.#database = JSON.parse(data)
+        })
+            .catch(() => {
+                this.#persist()
+            })
+    }
+
+    #persist() {
+        fs.writeFile(databasePath, JSON.stringify(this.#database))
+    }
 
     select(table) {
         const data = this.#database[table] ?? []
@@ -15,6 +30,8 @@ export class Database {
         } else {
             this.#database[table] = [data]
         }
+
+        this.#persist();
 
         return data;
     }
