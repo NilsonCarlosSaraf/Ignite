0d94027947e16ebac772bd304ea882b38ee13b95
NilsonCarlosSaraf <sarafnilson@gmail.com>
2024-02-12 14:36:24 -0300

    update

--- nodejs/01-fundamentos-nodejs/src/routes.js
+++ nodejs/01-fundamentos-nodejs/src/routes.js
@@ -33,6 +33,22 @@ export const routes = [
 
     },
     {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'), //toda vez que voce tiver ":" no caminho da rota significa que o parametro sera dinamico
+        handler: (req, res) => {
+            const { id } = req.params
+            const { name, email } = req.body
+
+            database.update('users', id, {
+                name,
+                email,
+            })
+
+            return res.writeHead(204).end()
+        },
+
+    },
+    {
         method: 'DELETE',
         path: buildRoutePath('/users/:id'), //toda vez que voce tiver ":" no caminho da rota significa que o parametro sera dinamico
         handler: (req, res) => {
