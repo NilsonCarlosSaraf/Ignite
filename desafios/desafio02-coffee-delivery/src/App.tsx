3bbc0e335293473affeb353890dae86b0ae58cdd
NilsonCarlosSaraf <sarafnilson@gmail.com>
2024-03-21 19:41:04 -0300

    set-up eslint

--- desafios/desafio02-coffee-delivery/src/App.tsx
+++ desafios/desafio02-coffee-delivery/src/App.tsx
@@ -1,10 +1,7 @@
-import { Router } from "./Router"
+import { Router } from './Router'
 
 function App() {
-
-  return (
-   <Router />
-  )
+  return <Router />
 }
 
 export default App
--- desafios/desafio02-coffee-delivery/src/components/BodyBanner.tsx
+++ desafios/desafio02-coffee-delivery/src/components/BodyBanner.tsx
@@ -1,41 +1,51 @@
-import { Coffee, Package, ShoppingCart, Timer } from "@phosphor-icons/react";
-import Cup from "../images/CoffeDeliveryCup.png"
+import { Coffee, Package, ShoppingCart, Timer } from '@phosphor-icons/react'
+import Cup from '../images/CoffeDeliveryCup.png'
 
 export function BodyBanner() {
-    return (
-        <div className="flex">
-            <div>
-                <div>
-                    <h1 className="font-extrabold text-4xl">Encontre o cafe perfeito para qualquer hora do dia</h1>
-                    <h3>Com o Coffe Delivery voce recebe seu cafe onde estiver, a qualquer hora</h3>
-                </div>
-                <div className="flex">
-                    <div>
-                        <span className="flex"><div className="bg-[#C47F17] rounded-full w-7 h-7 flex items-center justify-center">
-                            <ShoppingCart color="white" />
-                        </div>
-                            <h1>Compra simples e segura</h1></span>
-                        <span className="flex">
-                            <div className="bg-[#DBAC2C] rounded-full w-7 h-7 flex items-center justify-center">
-                                <Timer color="white" />
-                            </div> <h1>Compra simples e segura</h1></span>
-                    </div>
-                    <div>
-                        <span className="flex">
-                            <div className="bg-[#574F4D] rounded-full w-7 h-7 flex items-center justify-center">
-                                <Package color="white" />
-                            </div>
-                            <h1>Compra simples e segura</h1>
-                        </span>
-                        <span className="flex">
-                            <div className="bg-[#8047F8] rounded-full w-7 h-7 flex items-center justify-center">
-                                <Coffee color="white" className="bg-[#8047F8] rounded-full" />
-                            </div> <h1>Compra simples e segura</h1></span>
-                    </div>
-                </div>
-            </div>
-            <img src={Cup} />
-
+  return (
+    <div className="flex">
+      <div>
+        <div>
+          <h1 className="font-extrabold text-4xl">
+            Encontre o cafe perfeito para qualquer hora do dia
+          </h1>
+          <h3>
+            Com o Coffe Delivery voce recebe seu cafe onde estiver, a qualquer
+            hora
+          </h3>
         </div>
-    )
+        <div className="flex">
+          <div>
+            <span className="flex">
+              <div className="bg-[#C47F17] rounded-full w-7 h-7 flex items-center justify-center">
+                <ShoppingCart color="white" />
+              </div>
+              <h1>Compra simples e segura</h1>
+            </span>
+            <span className="flex">
+              <div className="bg-[#DBAC2C] rounded-full w-7 h-7 flex items-center justify-center">
+                <Timer color="white" />
+              </div>{' '}
+              <h1>Compra simples e segura</h1>
+            </span>
+          </div>
+          <div>
+            <span className="flex">
+              <div className="bg-[#574F4D] rounded-full w-7 h-7 flex items-center justify-center">
+                <Package color="white" />
+              </div>
+              <h1>Compra simples e segura</h1>
+            </span>
+            <span className="flex">
+              <div className="bg-[#8047F8] rounded-full w-7 h-7 flex items-center justify-center">
+                <Coffee color="white" className="bg-[#8047F8] rounded-full" />
+              </div>{' '}
+              <h1>Compra simples e segura</h1>
+            </span>
+          </div>
+        </div>
+      </div>
+      <img src={Cup} />
+    </div>
+  )
 }
