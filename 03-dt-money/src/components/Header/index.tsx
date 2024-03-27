9fec6a92c0940b599ca6ec7b8024686f0832989e
NilsonCarlosSaraf <sarafnilson@gmail.com>
2024-03-27 07:24:12 -0300

    update

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModal } from "../NewTransactionModal";
 
+
+//  o asChild no Dialog.Trigger asChild , nesse caso, vai fazer o componente reaproveitar o bottao dentro dele e nao criar um novo botao conforme bem da biblioteca. Sem o elemento asChild, um novo botao seria criado
 export function Header() {
     return (<HeaderContainer>
         <HeaderContent>
             <img src={logoImg} alt="" />
-            <NewTransactionButton>Nova Transacao</NewTransactionButton>
+            <Dialog.Root>
+                <Dialog.Trigger asChild>
+                    <NewTransactionButton>Nova Transacao</NewTransactionButton>
+                </Dialog.Trigger>
+
+                <NewTransactionModal />
+            </Dialog.Root>
         </HeaderContent>
     </HeaderContainer>)
 
